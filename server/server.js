5568bcb6ee2993770e5abcec19dd52153ae8c0b8
shivampanwar <panwar.shivam199@gmail.com>
2018-08-28 12:46:58 +0530

    added sockets

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const path=require('path')
+const http =  require('http')
 const express=require('express')
+const socketIO =  require('socket.io')
 
 var app=express()
 const publicPath=path.join(__dirname,'../public')
 const port=process.env.PORT||3000;//done for heroku
 // console.log(__dirname+'/../public')
-console.log(publicPath)
-
+// console.log(publicPath)
+var server =  http.createServer(app)
+var io=socketIO(server)//at this point we are ready to accept new connections
+io.on('connection',(socket)=>{
+   console.log('New user connected')
+})
+console.log('JJk')
 app.use(express.static(publicPath))
-app.listen(port,()=>{
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`)
 })
