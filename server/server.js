1715d5689d273c1c5232ad77df2f05c2c5f6e3a9
shivampanwar <panwar.shivam199@gmail.com>
2018-08-28 15:07:28 +0530

    Added new message and added custom event

--- server/server.js
+++ server/server.js
@@ -11,7 +11,25 @@ const port=process.env.PORT||3000;//done for heroku
 var server =  http.createServer(app)
 var io=socketIO(server)//at this point we are ready to accept new connections
 io.on('connection',(socket)=>{
-   console.log('New user connected')
+//    console.log('New user connected')
+//    socket.emit('newEmail',{
+//        from: "mike@example.com",
+//        text:"Hey, what is going omn",
+//        createAt: 123
+//    });
+//    socket.on('createEmail',(newEmail)=>{
+//        console.log("createEmail",newEmail)
+//    })
+   socket.on('createMessage',(createmessage)=>{
+       console.log("createMessage",createmessage)
+
+   })
+  
+   socket.emit('newMessage',{
+       from:"Shivam2gmail.com",
+       text:"Hii there buddy",
+       createdAt:"Today afternoon"
+   })
 })
 console.log('JJk')
 app.use(express.static(publicPath))
